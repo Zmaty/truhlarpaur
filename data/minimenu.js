c9cad49f5aa604a0d1988dc3fc232368cdf8e272
Zmaty <matypaur@gmail.com>
2025-10-16 15:50:40 +0200

    Delete data directory

--- data/minimenu.js
+++
@@ -1,7 +0,0 @@
-// optimalizace pro mobily
-let menu = document.querySelector('#menu');
-
-document.querySelector('.minimenu').onclick = function(){
-this.classList.toggle('active');
-menu.classList.toggle('active');
-};
